34e5f2bdf402e87456a3dacc22b864ddf5bec6d6
radionov <boss@flagstudio.ru>
2018-11-07 12:24:24 +0500

    init

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Flagstudio',
+            'email' => 'forspam@flagstudio.ru',
+            'password' => bcrypt('123123'), // secret
+             'email_verified_at' => now()
+            // 'remember_token' => str_random(10),
+        ]);    }
+}
