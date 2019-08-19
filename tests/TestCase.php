4d0cfb3eb7303f33eb39e42f09861b0437390fe4
vasishakd <vasishak.d@gmail.com>
2019-08-19 12:27:51 +0500

    FLG-535

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,27 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function signIn($user = null)
+    {
+        $user = $user ?: create('App\User');
+        $this->actingAs($user);
+        return $this;
+    }
+
+    /**
+     * Make ajax POST request
+     */
+    protected function ajaxPost($uri, array $data = [])
+    {
+        return $this->post($uri, $data, array('HTTP_X-Requested-With' => 'XMLHttpRequest'));
+    }
+
+    /**
+     * Make ajax GET request
+     */
+    protected function ajaxGet($uri, array $data = [])
+    {
+        return $this->call('GET', $uri, $data, [], [], ['HTTP_X-Requested-With' => 'XMLHttpRequest']);
+    }
 }
