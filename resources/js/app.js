de6faff7b2256cf76d070d19708781c8f07c27f7
rusakov <rusakov@flagstudio.ru>
2020-12-15 14:04:14 +0500

    add focus visible

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import axios from 'axios';
 import PersonalWarning from './components/personal-warning.vue';
+import 'focus-visible/dist/focus-visible.min.js';
 
 window.csrf = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 axios.defaults.headers.post['X-CSRF-Token'] = window.csrf;
