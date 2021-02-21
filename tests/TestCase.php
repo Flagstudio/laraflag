557228cd4ea7f50837801688c22f83a6219f0957
radionov <boss@flagstudio.ru>
2021-02-21 20:03:44 +0500

    fixes for autotests from smarte

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,18 +2,43 @@
 
 namespace Tests;
 
-use App\Models\User;
+use Closure;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\SQLiteBuilder;
+use Illuminate\Database\SQLiteConnection;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Fluent;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use RefreshDatabase;
 
+    public function __construct(?string $name = null, array $data = [], string $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+        $this->fixSqliteDropForeignKey();
+    }
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->seed('SettingsSeeder');
+    }
+
+    /**
+     * @param null $user
+     *
+     * @return \App\User
+     */
     protected function signIn($user = null)
     {
-        $user = $user ?: User::factory()->create();
+        $user = $user ?: create('App\User');
         $this->actingAs($user);
-        return $this;
+
+        return $user;
     }
 
     /**
@@ -34,5 +59,30 @@ abstract class TestCase extends BaseTestCase
     protected function ajaxGet($uri, array $data = [])
     {
         return $this->call('GET', $uri, $data, [], [], ['HTTP_X-Requested-With' => 'XMLHttpRequest']);
+    }
+
+    public function fixSqliteDropForeignKey()
+    {
+        \Illuminate\Database\Connection::resolverFor('sqlite', function ($connection, $database, $prefix, $config) {
+            return new class($connection, $database, $prefix, $config) extends SQLiteConnection {
+                public function getSchemaBuilder()
+                {
+                    if ($this->schemaGrammar === null) {
+                        $this->useDefaultSchemaGrammar();
+                    }
+                    return new class($this) extends SQLiteBuilder {
+                        protected function createBlueprint($table, Closure $callback = null)
+                        {
+                            return new class($table, $callback) extends Blueprint {
+                                public function dropForeign($index)
+                                {
+                                    return new Fluent();
+                                }
+                            };
+                        }
+                    };
+                }
+            };
+        });
     }
 }
